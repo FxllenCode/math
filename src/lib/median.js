31842ab55ade8995b28e63fd09850cfc2e65c918
eswebber07 <40151425+eswebber07@users.noreply.github.com>
2021-10-14 15:28:57 -0400

    fixe

--- src/lib/median.js
+++ src/lib/median.js
@@ -1,3 +1,9 @@
+const floatUtil = require('./utils/isFloat');
+
 module.exports = function median(...numbers) {
-    
-}
+  const amount = numbers.length;
+  let final = 0;
+  if (floatUtil.isFloat(amount / 2)) {
+    final = Math.floor(amount / 2) + 1;
+  }
+};
